03ba4c07d5b155cef07ae3b59939306cdf9c4f2b
Rohit Deshmukh <rohit.deshmukh@synerzip.com>
2015-08-10 04:42:34 +0000

    Added new IP of AWS

--- client/app.js
+++ client/app.js
@@ -10,4 +10,4 @@ var app = angular.module('peninsula', [
 
     $locationProvider.html5Mode(true);
 });
-app.constant('host',"http://52.24.218.178:3600");
+app.constant('host',"http://52.27.109.3:3600");
