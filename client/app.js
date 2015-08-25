61383599d32f7c9cde63e2a242e3e724f9a40a85
Rohit Deshmukh <rohit.deshmukh@synerzip.com>
2015-08-25 04:25:29 +0000

    app.js edited online with Bitbucket

--- client/app.js
+++ client/app.js
@@ -10,4 +10,4 @@ var app = angular.module('peninsula', [
 
     $locationProvider.html5Mode(true);
 });
-app.constant('host',"http://54.148.50.250:3600");
+app.constant('host',"http://54.191.207.203:3600");
