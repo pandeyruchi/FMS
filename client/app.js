5a987fc43d9ea4d43b10efa987c7b5e8021bb579
Rohit Deshmukh <rohit.deshmukh@synerzip.com>
2015-09-02 05:14:43 +0000

    app.js edited online with Bitbucket

--- client/app.js
+++ client/app.js
@@ -10,4 +10,4 @@ var app = angular.module('pune', [
 
     $locationProvider.html5Mode(true);
 });
-app.constant('host',"http://52.88.22.96:3600");
+app.constant('host',"http://52.10.132.128:3600");
